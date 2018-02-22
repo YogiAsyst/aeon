fafe7a0afeff5408e5522b01ef3f969e53f84a9a
Yogi <yogi.agsena@asyst.co.id>
2018-02-22 12:36:33 +0000

    create pinjaman page New files added - [kredit_main_page,login_page,pinjaman_page,FacebookOAuth,GoogleOAuth,LinkedInOAuth,googlemaps,imagelists] Files modified - [Login,Main,footer,header,topnav,app.variables.json,pages-config.json]

---
+++ src/main/webapp/WEB-INF/prefabs/FacebookOAuth/webapp/wmProperties.js
@@ -0,0 +1,12 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "default",
+  "defaultLanguage" : "en",
+  "displayName" : "facebook",
+  "homePage" : "Main",
+  "name" : "facebook",
+  "platformType" : "DEFAULT",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "type" : "PREFAB",
+  "version" : "1.2"
+};
---
+++ src/main/webapp/pages/login_page/login_page.js
@@ -0,0 +1,29 @@
+Application.$controller("login_pagePageController", ["$scope", function ($scope) {
+    "use strict";
+
+    /* perform any action on widgets within this block */
+    $scope.onPageReady = function () {
+        /*
+         * widgets can be accessed through '$scope.Widgets' property here
+         * e.g. to get value of text widget named 'username' use following script
+         * '$scope.Widgets.username.datavalue'
+         */
+    };
+
+}]);
+
+
+Application.$controller("login1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
+
+
+Application.$controller("loginFormController", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
